d748bf1ba88039ff526fd6ab579c180ab443ffec
Ritika Das <61988162+Ritika-Das@users.noreply.github.com>
2020-08-22 23:26:27 +0530

    Create Noughts and Crosses.c

---
+++ Noughts and Crosses.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char arr[3][3];
+    printf("Welcome to Noughts and Crosses: Let's proceed with the white!");
+    printf("Choose your block:");
+    scanf("%d",&n);
+}
